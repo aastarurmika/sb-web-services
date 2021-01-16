1efbd2ace5afb7bd0c20f6f6c148a5e5db716dff
Ankit <ankitv89@gmail.com>
2021-01-16 08:48:37 +0530

    overview api added

--- src/publicApi_v8/home.ts
+++ src/publicApi_v8/home.ts
@@ -8,12 +8,16 @@ import { logError } from '../utils/logger'
 const GENERAL_ERROR_MSG = 'Failed due to unknown reason'
 import { processContent } from '../utils/contentHelpers'
 
+import { getContentDetails} from '../protectedApi_v8/content'
 import { getFilters } from '../service/catalog'
 import { CONSTANTS } from '../utils/env'
 import { ERROR } from '../utils/message'
 const API_END_POINTS = {
-  searchV6: `${CONSTANTS.SEARCH_API_BASE}/v6/search`,
+  searchAutoComplete: `${CONSTANTS.ES_BASE}`,
+  searchV6: `${CONSTANTS.SEARCH_API_BASE}/v6/search`
+  
 }
+
 export const homePage = Router()
 
 const adminId = 'ec2687b9-7b86-4321-bbc7-8c9509b834ee'
@@ -114,43 +118,116 @@ homePage.get('/catalog', async (req, res) => {
   }
 })
 
-// homePage.get('/popularCourses', async (req, res) => {
-//   try {
+homePage.post('/:contentId', async (req, res) => {
+  try {
+    const org = req.header('org')
+    const rootOrg = req.header('rootOrg')
+    if (!org || !rootOrg) {
+      res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
+      return
+    }
+    const { contentId } = req.params
+    const additionalFields = req.body.additionalFields
+    const fetchOneLevel = req.body.fetchOneLevel || false
+    const hierarchyType = req.query.hierarchyType
+    const response = await getContentDetails(
+      contentId,
+      rootOrg,
+      org,
+      adminId,
+      hierarchyType,
+      additionalFields,
+      fetchOneLevel
+    )
 
-//   } catch (err) {
-//     logError('HomePage API >', err)
-//   }
-// })
+    res.json(response)
+  } catch (err) {
+    res
+      .status((err && err.response && err.response.status) || 500)
+      .send((err && err.response && err.response.data) || {
+        error: GENERAL_ERROR_MSG,
+      })
+  }
+})
 
-// publicTnc.get('/', async (req, res) => {
-//   try {
-//     const rootOrg = req.header('rootOrg') || ''
-//     const org = req.header('org') || ''
-//     let locale = 'en'
-//     if (!org || !rootOrg) {
-//       res.status(400).send(ERROR.ERROR_NO_ORG_DATA)
-//       return
-//     }
-//     if (req.query.locale) {
-//       locale = req.query.locale
-//     }
-//     const response = await axios({
-//       ...axiosRequestConfig,
-//       headers: {
-//         langCode: locale,
-//         org,
-//         rootOrg,
-//       },
-//       method: 'GET',
-//       url: apiEndpoints.tnc,
-//     })
-//     res.json(response.data)
-//   } catch (err) {
-//     logError('TNC ERR >', err)
-//     res.status((err && err.response && err.response.status) || 500).send(
-//       (err && err.response && err.response.data) || {
-//         error: 'Failed due to unknown reason',
-//       }
-//     )
-//   }
-// })
+homePage.get('/searchAutoComplete', async (req, res) => {
+  try {
+    const rootOrg = req.header('rootOrg')
+    const org = req.header('org')
+    const query = req.query.q
+    const lang = req.query.l
+    // tslint:disable-next-line: no-any
+    const body: any = {
+      _source: ['searchTerm'],
+      query: {
+        bool: {
+          filter: [
+            {
+              term: {
+                rootOrg,
+              },
+            },
+            {
+              term: {
+                org,
+              },
+            },
+          ],
+          should: !query.length
+            ? undefined
+            : [
+              {
+                prefix: {
+                  'searchTermAnalysed.keyword': {
+                    boost: 4,
+                    value: query,
+                  },
+                },
+              },
+              {
+                prefix: {
+                  searchTermAnalysed: {
+                    boost: 2,
+                    value: query,
+                  },
+                },
+              },
+            ],
+        },
+      },
+      size: 20,
+    }
+    const isSuggestedTerm = {
+      term: {
+        isSuggested: true,
+      },
+    }
+    if (!query.length) {
+      body.query.bool.filter.push(isSuggestedTerm)
+    }
+    const response = await axios.request({
+      auth: {
+        password: CONSTANTS.ES_PASSWORD,
+        username: CONSTANTS.ES_USERNAME,
+      },
+      data: body,
+      method: 'POST',
+      ...axiosRequestConfig,
+      url: `${API_END_POINTS.searchAutoComplete}/searchautocomplete_${lang}/autocomplete/_search`,
+    })
+    let data = []
+    if (response.data && response.data.hits && response.data.hits.hits) {
+      data = response.data.hits.hits.filter((result: { _source: { searchTerm: string } }) => {
+        return result._source.searchTerm.length
+      })
+    }
+    res.json(data)
+  } catch (err) {
+    logError('SEARCH AUTOCOMPLETE ERR -> ', err)
+    res
+      .status((err && err.response && err.response.status) || 500)
+      .send((err && err.response && err.response.data) || {
+        error: GENERAL_ERROR_MSG,
+      })
+  }
+})
